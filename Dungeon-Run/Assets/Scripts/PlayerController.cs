37f62216133067480cea70c2dcba14d1ebedef48
Wolf1596Games <wolf1596games@gmail.com>
2021-01-28 16:46:09 -0600

    Player movement set up

--- Dungeon-Run/Assets/Scripts/PlayerController.cs
+++ Dungeon-Run/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement Variables")]
+    [SerializeField] float baseMovementSpeed = 1f;
+    [SerializeField] float speed = 0f;
+    [SerializeField] float sprintMultiplier = 1.2f;
+    private float horizontalMovement = 0f;
+    private float verticalMovement = 0f;
+
+
+    Rigidbody2D rb;
+    BoxCollider2D playerCollider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = FindObjectOfType<Rigidbody2D>();
+        playerCollider = FindObjectOfType<BoxCollider2D>();
+
+        speed = baseMovementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Sprinting
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed *= sprintMultiplier;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = baseMovementSpeed;
+        }
+
+        //Horizontal Movement
+        horizontalMovement = Input.GetAxis("Horizontal");
+
+        if(horizontalMovement > 0f)
+        {
+            rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+        }
+        else if(horizontalMovement < 0f)
+        {
+            rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        //Vertical Movement
+        verticalMovement = Input.GetAxis("Vertical");
+
+        if(verticalMovement > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, verticalMovement * speed);
+        }
+        else if(verticalMovement < 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, verticalMovement * speed);
+        }
+        else
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+        }
     }
 }

7822bf28c039261798d26edc79aa03a303f9b783
Wolf1596Games <wolf1596games@gmail.com>
2021-02-08 00:15:13 -0600

    Added scripts for isometric movement

---
+++ Dungeon-Run/Assets/Scripts/IsomeetricPlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsomeetricPlayerController : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    [SerializeField] float baseMovementSpeed = 8f;
+    [SerializeField] float speed = 0f;
+    [SerializeField] float sprintMultiplier = 1.75f;
+    [SerializeField] float dodgeDuration = .5f;
+    [SerializeField] float dodgeCooldown = 5f;
+    [SerializeField] bool sprinting = false;
+
+    [Header("Player Health")]
+    [SerializeField] public int maxHealth = 3;
+    [SerializeField] public int currentHealth = 3;
+
+    [Header("Player Fighting")]
+    [SerializeField] public int damage = 1;
+    [SerializeField] float swingRange = 1.5f;
+    [SerializeField] float timeBetweenSwings = .35f;
+    [SerializeField] float timeBetweenShots = .2f;
+
+    [Header("Projectile")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] float projectileSpeed = 6.5f;
+
+    //Private movement variables
+    private float horizontalMovement = 0f;
+    private float verticalMovement = 0f;
+    private float timeSinceDodge = 5f;
+
+    //References
+    private IsometricCharacterRenderer isoRenderer;
+    private Rigidbody2D rb;
+    private Camera main;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        main = FindObjectOfType<Camera>();
+
+        //Set speed to base speed
+        speed = baseMovementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentPos = rb.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * speed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        //isoRend
+    }
+}
---
+++ Dungeon-Run/Assets/Scripts/IsometricCharacterRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricCharacterRenderer : MonoBehaviour
+{
+    public static readonly string[] staticDirections = { "Static N", "Static NW", "Static W", "Static SW", "Static S", "Static SE", "Static E", "Static NE" };
+    public static readonly string[] runDirections = { "Run N", "Run NW", "Run W", "Run SW", "Run S", "Run SE", "Run E", "Run NE" };
+
+    Animator animator;
+    int lastDirection;
+
+    private void Awake()
+    {
+        //cache the animator component
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetDirection(Vector2 direction)
+    {
+        //use the Run states by default
+        string[] directionArray = null;
+
+        //measure the magnitude of the input
+        if(direction.magnitude < .01f)
+        {
+            //if we are basically standing still, we'll use the Static states
+            directionArray = staticDirections;
+        }
+        else
+        {
+            directionArray = runDirections;
+            //lastDirection = DirectionToIndex(direction, 8);
+        }
+
+        animator.Play(directionArray[lastDirection]);
+    }
+}

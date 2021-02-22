1feffc79272480e66f7cf91601f9f42e32c7d89e
Wolf1596Games <wolf1596games@gmail.com>
2021-02-22 16:13:26 -0600

    Added astralPlane bool to GameManager

--- Dungeon-Run/Assets/Scripts/GameManager.cs
+++ Dungeon-Run/Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Plane Management")]
+    [Tooltip("Controls which \"plane\" the player is in.")]
+    public bool astralPlane = false;
+
     // Start is called before the first frame update
     void Start()
     {

f7577a75d7dca18049a794d41355b5b21a3b1089
Wolf1596Games <wolf1596games@gmail.com>
2021-02-24 16:26:43 -0600

    Added RoomManager script.

--- Dungeon-Run/Assets/Temp/TestDummy.cs
+++ Dungeon-Run/Assets/Temp/TestDummy.cs
@@ -6,12 +6,31 @@ public class TestDummy : MonoBehaviour
 {
     [SerializeField] int health = 3;
 
+    RoomManager[] rooms;
+    public RoomManager activeRoom;
+
+    private void Start()
+    {
+        //Determines which room the dummy (enemy) is in
+        rooms = FindObjectsOfType<RoomManager>();
+
+        foreach(RoomManager room in rooms)
+        {
+            if(Vector2.Distance(transform.position, room.transform.position) < 10)
+            {
+                activeRoom = room;
+            }
+        }
+    }
+
     public void TakeDamage(int damage)
     {
         health -= damage;
 
         if(health <= 0)
         {
+            activeRoom.EnemyKilled();
+
             Destroy(gameObject);
         }
     }

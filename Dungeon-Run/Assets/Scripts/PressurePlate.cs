ae94ce224736157f9f0d9cdb424e300622e6cb76
Wolf1596Games <wolf1596games@gmail.com>
2021-02-26 17:22:10 -0600

    Added scripts for a pressure plate and switch

---
+++ Dungeon-Run/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public bool active = false;
+    public bool previouslyActivated = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine("Activation");
+        }
+    }
+
+    private IEnumerator Activation()
+    {
+        active = true;
+
+        yield return new WaitForEndOfFrame();
+
+        active = false;
+        previouslyActivated = true;
+    }
+}
---
+++ Dungeon-Run/Assets/Scripts/Switch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public bool playerInRange = false;
+    public bool active = false;
+
+    private SpriteRenderer sprRenderer;
+
+    private void Awake()
+    {
+        sprRenderer = GetComponent<SpriteRenderer>();
+
+        sprRenderer.color = Color.yellow;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(playerInRange && Input.GetKeyDown(KeyCode.E) && !active)
+        {
+            active = true;
+            sprRenderer.color = Color.green;
+        }
+        else if(playerInRange && Input.GetKeyDown(KeyCode.E) && active)
+        {
+            active = false;
+            sprRenderer.color = Color.yellow;
+        }
+    }
+}

c185cd365a98922ad089fd6647bc14abe38ddc91
Wolf1596Games <wolf1596games@gmail.com>
2021-03-11 01:42:33 -0600

    Projectiles instantiate in any direction
    Need to figure out why it isn't moving.

--- Dungeon-Run/Assets/Scripts/Player/PlayerProjectile.cs
+++ Dungeon-Run/Assets/Scripts/Player/PlayerProjectile.cs
@@ -8,13 +8,17 @@ public class PlayerProjectile : MonoBehaviour
     [SerializeField] float lifespan = 10f;
     [Tooltip("Time since the object was instantiated. FOR DEBUG ONLY")]
     [SerializeField] float timeSinceCreated = 0f;
+    [Tooltip("Projectile velocity")]
+    [SerializeField] float moveSpeed = 75f;
 
     IsometricPlayerController player;
+    Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<IsometricPlayerController>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,8 +28,13 @@ public class PlayerProjectile : MonoBehaviour
 
         if(timeSinceCreated >= lifespan)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
+    }
+
+    public void Setup(Vector3 shootDir)
+    {
+        rb.AddForce(shootDir * moveSpeed, ForceMode2D.Impulse);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)

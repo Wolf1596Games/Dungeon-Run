89f565d4a4c4720dda96a3605f3fc05f0ed0a56d
Wolf1596Games <wolf1596games@gmail.com>
2021-03-11 19:32:00 -0600

    Added transportation to the "astral plane"
    Also added sample prefabs

--- Dungeon-Run/Assets/Scripts/GameManager.cs
+++ Dungeon-Run/Assets/Scripts/GameManager.cs
@@ -8,10 +8,13 @@ public class GameManager : MonoBehaviour
     [Header("Plane Management")]
     [Tooltip("Controls which \"plane\" the player is in.")]
     public bool astralPlane = false;
+    [Tooltip("Name of the scene the player should be transferred to upon death")]
+    [SerializeField] private string astralPlaneScene;
 
-    public IsometricPlayerController[] players;
+    private IsometricPlayerController[] players;
     public IsometricPlayerController activePlayer;
     public int currentSceneIndex;
+    public int lastSceneIndex;
 
     Scene scene;
 
@@ -47,7 +50,11 @@ public class GameManager : MonoBehaviour
 
     public void ToAstralPlane()
     {
-        SceneManager.LoadScene("TestScene");
+        StartCoroutine("AstralPlaneCoroutine");
+    }
+    public void FromAstralPlane()
+    {
+        StartCoroutine("FromAstralPlaneCoroutine");
     }
     public void GameOver()
     {
@@ -58,8 +65,28 @@ public class GameManager : MonoBehaviour
         StartCoroutine("NextLevelCoroutine");
     }
 
+    private IEnumerator AstralPlaneCoroutine()
+    {
+        lastSceneIndex = currentSceneIndex;
+        SceneManager.LoadScene(astralPlaneScene);
+
+        yield return new WaitForSeconds(1f);
+
+        players = GetPlayers();
+        ChooseActivePlayer();
+    }
+    private IEnumerator FromAstralPlaneCoroutine()
+    {
+        SceneManager.LoadScene(lastSceneIndex);
+
+        yield return new WaitForSeconds(1f);
+
+        players = GetPlayers();
+        ChooseActivePlayer();
+    }
     private IEnumerator NextLevelCoroutine()
     {
+        lastSceneIndex = currentSceneIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
 
         yield return new WaitForSeconds(1f);

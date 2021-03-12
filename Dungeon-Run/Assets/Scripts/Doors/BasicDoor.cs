7a607300727e8cac7e3f1df94077b07a441b969f
Wolf1596Games <wolf1596games@gmail.com>
2021-03-11 23:38:47 -0600

    Added basic door script

---
+++ Dungeon-Run/Assets/Scripts/Doors/BasicDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDoor : MonoBehaviour
+{
+    [SerializeField] private Switch activatorSwitch;
+    [SerializeField] private string triggerName;
+
+    private Collider2D collider2d;
+    private Animator animator;
+
+    private void Awake()
+    {
+        collider2d = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(activatorSwitch.active)
+        {
+            collider2d.isTrigger = true;
+            animator.SetTrigger(triggerName);
+        }
+    }
+}

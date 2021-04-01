96f2133efbcc62d0a1c7b739c46f568303dc107f
Wolf1596Games <wolf1596games@gmail.com>
2021-04-01 18:38:28 -0500

    Made IsometricCharacterRenderer octa-directional

--- Dungeon-Run/Assets/Scripts/Player/IsometricCharacterRenderer.cs
+++ Dungeon-Run/Assets/Scripts/Player/IsometricCharacterRenderer.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class IsometricCharacterRenderer : MonoBehaviour
 {
-    public static readonly string[] staticDirections = { "Static W", "Static E" };
-    public static readonly string[] runDirections = { "Run W", "Run E" };
+    public static readonly string[] staticDirections = { "Static N", "Static MW", "Static W", "Static SW", "Static S", "Static SE", "Static E", "Static NE" };
+    public static readonly string[] runDirections = { "Run N", "Run MW", "Run W", "Run SW", "Run S", "Run SE", "Run E", "Run NE" };
 
     Animator animator;
     int lastDirection;

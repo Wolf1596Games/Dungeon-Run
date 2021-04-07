b4458abfa660ef8d58b70c3caee2a4e1c1b80495
Wolf1596Games <wolf1596games@gmail.com>
2021-04-06 23:37:20 -0500

    Added MusicPlayer
    Also fixed UI

---
+++ Dungeon-Run/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if (audioSource.clip != null)
+        {
+            audioSource.Play();
+        }
+    }
+}

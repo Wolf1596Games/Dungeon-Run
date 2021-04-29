aec2bb8255ce2d777df3afe8726b5af03dc2757a
DanJGV <dangv93@gmail.com>
2021-04-29 18:31:07 -0500

    Boss_2_Update'

--- Dungeon-Run/Assets/Scripts/Enemy Scripts/Boss_2.cs
+++ Dungeon-Run/Assets/Scripts/Enemy Scripts/Boss_2.cs
@@ -2,17 +2,119 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss_2 : Type1
+public class Boss_2 : Enemy
 {
+    public Transform projectile;
+    public Transform target;
+    public Transform spawnPoint;
+    public Transform spawnPoint2;
+    public Transform spawnPoint3;
+    public float detectRad;
+    public float shootTimer;
+    public float inShootTimer;
+    public float waitTime;
+    public float startWaitTime;
+    public bool isCooling = false;
+    private Animator shooterAnim;
+    public float speed;
+    public Transform[] moveSpots;
+    private int randomSpot;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        target = GameObject.FindWithTag("Player").transform;
+        shooterAnim = GetComponent<Animator>();
+        currentState = EnemyState.idle;
+        currentHealth = maxHealth;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        CheckDistance();
+        Strafe();
+    }
+
+    void Strafe()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    void CheckDistance()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= detectRad && isCooling == false)
+        {
+            Shoot();
+
+        }
+        else if (currentState == EnemyState.attack && isCooling)
+        {
+            Cooldown();
+        }
+
+
+    }
+
+    void Shoot()
+    {
+        ChangeState(EnemyState.attack);
+        shooterAnim.SetBool("isAttacking", true);
+        shooterAnim.SetFloat("moveX", (target.position.x - transform.position.x));
+        shooterAnim.SetFloat("moveY", (target.position.y - transform.position.y));
+    }
+    void Cooldown()
+    {
+
+        shootTimer -= Time.deltaTime;
+        if (shootTimer <= 0)
+        {
+            isCooling = false;
+            shootTimer = inShootTimer;
+        }
+    }
+    void TriggerArrow()
+    {
+        if (shootTimer == inShootTimer)
+        {
+            Transform arrow = Instantiate(projectile, spawnPoint.transform.position, Quaternion.identity);
+            Transform arrow2 = Instantiate(projectile, spawnPoint2.transform.position, Quaternion.identity);
+            Transform arrow3 = Instantiate(projectile, spawnPoint3.transform.position, Quaternion.identity);
+            Vector3 shootDir = (spawnPoint.position - target.position).normalized;
+            Vector3 shootDir2 = (spawnPoint2.position - target.position).normalized;
+            Vector3 shootDir3 = (spawnPoint3.position - target.position).normalized;
+            arrow.GetComponent<Enemy_Projectile>().Launch(shootDir);
+            arrow2.GetComponent<Enemy_Projectile>().Launch(shootDir2);
+            arrow3.GetComponent<Enemy_Projectile>().Launch(shootDir3);
+            shooterAnim.SetBool("isAttacking", false);
+        }
+        isCooling = true;
+    }
+    public void Death()
+    {
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void ChangeState(EnemyState newState)
+    {
+        if (currentState != newState)
+        {
+            currentState = newState;
+        }
     }
 }

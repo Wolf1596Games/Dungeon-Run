673cc4ec0c281bfefcef38de896324739e8cfbbf
Wolf1596Games <wolf1596games@gmail.com>
2021-05-03 13:39:13 -0500

    Golem Corpse no longer hinders movement.

--- Dungeon-Run/Assets/Scripts/Interactables/GolemCorpse.cs
+++ Dungeon-Run/Assets/Scripts/Interactables/GolemCorpse.cs
@@ -7,8 +7,14 @@ public class GolemCorpse : MonoBehaviour
     public bool playerInRange = false;
     public float playerDetectionRange = 1f;
 
-    private GameManager manager;
+    private GameManager manager; 
+    private Collider2D corpseCollider;
     public Transform player;
+
+    private void Awake()
+    {
+        corpseCollider = GetComponent<Collider2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -35,10 +41,12 @@ public class GolemCorpse : MonoBehaviour
         if(playerInRange && Input.GetKeyDown(KeyCode.E) && transform.parent == null)
         {
             transform.parent = player;
+            corpseCollider.isTrigger = true;
         }
         else if(playerInRange && Input.GetKeyDown(KeyCode.E) && transform.parent != null)
         {
             transform.parent = null;
+            corpseCollider.isTrigger = false;
         }
     }
 }

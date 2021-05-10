4fa312aebc584a81e31c94ed361c560233ec65fc
DanJGV <dangv93@gmail.com>
2021-05-10 17:21:51 -0500

    Boss1Update

--- Dungeon-Run/Assets/Scripts/Enemy Scripts/Boss_1.cs
+++ Dungeon-Run/Assets/Scripts/Enemy Scripts/Boss_1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Boss_1 : Type1
 {
+    public GameObject ramp;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,14 @@ public class Boss_1 : Type1
     // Update is called once per frame
     void Update()
     {
-        
+        ActivateRamp();
+    }
+
+    void ActivateRamp()
+    {
+        if(currentHealth <= 0)
+        {
+            ramp.SetActive(true);
+        }
     }
 }
